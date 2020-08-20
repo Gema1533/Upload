2d272cb60c9108760c2594da62349700f3d0ed72
Gema1533 <65255625+Gema1533@users.noreply.github.com>
2020-08-20 09:57:41 +0700

    Add files via upload

---
+++ Class.php
@@ -0,0 +1,27 @@
+<? php
+
+Class Gitar {
+//properties
+	public $warna;
+	public $jenis;
+	public $design;
+//method 
+	public function warna() {
+		$warna;
+	} 
+
+	public function jumlahJenis() {
+		$jenis;
+	} 
+	public function jumlahDesign() {
+		$design;
+	} 
+//instansiasi class
+	$gitar = new Gitar() ; 
+//memanggil
+	echo  $gitar->warna ;
+	echo  $gitar->jumlah Jenis ;
+	echo  $gitar->jumlahDesign ;
+		   
+	
+} 
